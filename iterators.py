1de7394c5fee5f984aaf121700be6afcdce2290a
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-06-16 23:14:40 +0800

    Learned how iterators work in python

---
+++ iterators.py
@@ -0,0 +1,60 @@
+"""Python Iterators
+An iterator is an object that contains a countable number of values.
+
+An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
+"""
+
+
+# Return an iterator from a tuple, and print each value:
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))#apple
+print(next(myit))#banana
+print(next(myit))#cherry
+
+# Strings are also iterable objects, containing a sequence of characters:
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+
+# Iterate the values of a tuple:
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+  
+  
+# Create an iterator that returns numbers, starting with 1, and each sequence will increase by one (returning 1,2,3,4,5 etc.):
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
